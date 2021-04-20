5da6d30e683dea198e8cdd5098219af121512694
Achraf <hardy1348@live.fr>
2021-04-20 14:36:42 +0200

    create

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -1,4 +1,4 @@
-<header id="masthead" class="site-header" data-anchor-target=".hero" data-top="background: rgba(59,58,54,0); border-bottom-color: rgba(226,226,226,0);" data-top-bottom="background: rgba(59,58,54,1); border-bottom-color: rgba(226,226,226,1);">
+<header id="masthead" class="site-header-fill" data-anchor-target=".hero" data-top="background: rgba(59,58,54,0); border-bottom-color: rgba(226,226,226,0);" data-top-bottom="background: rgba(59,58,54,1); border-bottom-color: rgba(226,226,226,1);">
 
     <nav id="primary-navigation" class="site-navigation">
 
@@ -13,7 +13,7 @@
             <div class="main-menu" id="perfect-navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="active">
-                        <a href="layouts.index">Home</a></li>
+                        <a href="/">Home</a></li>
                     <li>
                         <a href="/blog">Blog </a>
                     </li>
