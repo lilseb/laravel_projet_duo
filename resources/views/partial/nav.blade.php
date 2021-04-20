95d5df894a49aac6038272456ba0db33cfbecf9f
Sébastien <seb.lasante@hotmail.com>
2021-04-20 14:36:19 +0200

    seb2

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -1,36 +1,34 @@
-<header id="masthead" class="site-header" data-anchor-target=".hero" data-top="background: rgba(59,58,54,0); border-bottom-color: rgba(226,226,226,0);" data-top-bottom="background: rgba(59,58,54,1); border-bottom-color: rgba(226,226,226,1);">
+<header id="masthead" class="site-header site-header-fill" data-anchor-target=".hero" data-top="background: rgba(59,58,54,0); border-bottom-color: rgba(226,226,226,0);" data-top-bottom="background: rgba(59,58,54,1); border-bottom-color: rgba(226,226,226,1);">
 
     <nav id="primary-navigation" class="site-navigation">
-
+    
         <div class="container-fluid">
-
+    
             <div class="navbar-header">
                 
-                <a href="index.html" class="site-title yellow-text">Business Perfect</a>
-
+                <a href={{route("home")}} class="site-title yellow-text ">Business Perfect</a>
+    
             </div><!-- /.navbar-header -->
-
+    
             <div class="main-menu" id="perfect-navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li class="active">
-                        <a href="layouts.index">Home</a></li>
+                        <a href={{route("home")}}>Home</a></li>
                     <li>
-                        <a href="/blog">Blog </a>
+                        <a href={{route("blog")}}>Blog </a>
                     </li>
                     <li>
-                        <a href="/portfolio">Portfolio</a>                                
+                        <a href={{route("portfolio")}}>Portfolio</a>                                
                     </li>
                     <li>
-                        <a href="/contact">Contact</a>
+                        <a href={{route("contact")}}>Contact</a>
                     </li> 
                     <li>
-                        <a href="/backBlog">BackOffice</a>
+                        <a href={{route("backBlog")}}>Backblog</a>
                     </li> 
                 </ul><!-- /.navbar-nav -->
             </div><!-- /.navbar-collapse -->
             
         </div>
-                  
     </nav><!-- /.site-navigation -->
-    
-</header><!-- /#masthead -->
+</header>
